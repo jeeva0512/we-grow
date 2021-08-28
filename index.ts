437375917f6783e051cb27ea59d7b6808f1cc0a4
jeeva <jeevatamilarasi05@gmail.com>
2021-08-28 07:49:29 +0530

    1st commit

---
+++ index.ts
@@ -0,0 +1,4 @@
+//import { ActionReducerMap, MetaReducer } from '@ngrx/store';
+//import { IAppState } from './app/interface';
+
+//export const reducers: ActionReducerMap<IAppState>
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './views/login/login.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, LoginComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    StoreModule.forRoot({}, {}),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
